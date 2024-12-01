0c67b7d1307083e7f794a854cc8713f93388e7fd
Murilo NP <69391499+Nubi-Prey@users.noreply.github.com>
2024-12-01 03:31:28 +0000

    Adiciona a pagina inicial

---
+++ pages/index.js
@@ -0,0 +1,12 @@
+function Home(){
+    return (
+        <div>
+            <h1>
+                "Uva roxa, leu porque é trouxa"
+            </h1>
+            <h2>
+                — Eduardo, Carlos
+            </h2>
+        </div>
+    )
+}
